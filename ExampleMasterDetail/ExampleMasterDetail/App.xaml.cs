f306a6d20064b2785476279e44143b0320a79aef
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-21 20:40:13 -0500

    Agregar archivos de proyecto.

---
+++ ExampleMasterDetail/ExampleMasterDetail/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ExampleMasterDetail.Services;
+using ExampleMasterDetail.Views;
+
+namespace ExampleMasterDetail
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ExampleMasterDetail/ExampleMasterDetail/Models/HomeMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleMasterDetail.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        Example,
+        Example2
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
